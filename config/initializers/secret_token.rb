14ab661de02313993d3ec4a21a7fb55a1b6b33dd
Silas Barta <sbarta@gmail.com>
2012-03-12 10:43:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerclone::Application.config.secret_token = '4406dae502ead7186588cc2e34df87794d8e48dfd2c35d64d7700e5abb80de792012e0049ef799c90fa97810a285f63bb49e327bd71f19afd702bc91429fcf28'
